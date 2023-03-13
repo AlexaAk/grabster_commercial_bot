6ca4ec0ead2ad10978664dae833f0c6454b59639
AlexaAk <ahmatova.sasha@yandex.ru>
2023-03-13 19:09:43 +0300

    trying out threading & scheduling

--- src/grabster_commercial_bot/grabster_commercial_bot.py
+++ src/grabster_commercial_bot/grabster_commercial_bot.py
@@ -6,7 +6,10 @@ from enum import Enum
 import json
 import os
 import platform
+import schedule
+import time
 import requests
+import threading
 if platform.system() == "Linux":
     os.chdir("/home/alexaak/python/Buh_bots/")
 else:
@@ -45,5 +48,17 @@ def send_telegram(text: str):
         raise Exception("post_text error")
 
 
+def job():
+    send_telegram("сейчас 18:59")
+
+
+def thread_function():
+    schedule.every().day.at("18:59").do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 if __name__ == '__main__':
+    x = threading.Thread(target=thread_function)
     send_telegram("hello world!")
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,19 @@
-import json
-import os
-os.chdir("../")
+import schedule
+import time
+import threading
 
-jsonConfig = open("config.json", "r", encoding="utf-8")
-json_file = json.load(jsonConfig)
+def job():
+    print("I'm working...")
 
-print(len(json_file['list_employee_tg']))
-print(len(json_file['list_employee_names']))
-print(len(json_file['list_employee_cards']))
+
+def thread_function(name):
+    schedule.every().day.at("19:05").do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    x = threading.Thread(target=thread_function, args=(1,))
+    print("hello world!")
+    x.join()
